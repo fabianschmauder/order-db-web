bddd764236d11c9d2a25aa64df8f3a868bd1d1a4
fabianschmauder <fabianschmauder@gmail.com>
2020-05-27 15:16:21 +0200

    Init Project

---
+++ src/main/java/de/neuefische/orderdbweb/OrderDbWebApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderdbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDbWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrderDbWebApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/neuefische/orderdbweb/OrderDbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderdbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDbWebApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
